4a9c40e69a9c58b795d53bb8ecadccc6fa1b6265
Josh Kenn Viray <blasingkennviray@gmail.com>
2024-12-28 23:08:51 +0800

    initial  todo completed, no front end

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,70 @@
-import { MantineProvider, Box } from '@mantine/core';
-import useSWR from 'swr';
-import AddTodo from '../components/AddTodo';
-import './App.css';
+import { Box, List, ThemeIcon } from "@mantine/core";
+import { CheckCircleFillIcon } from "@primer/octicons-react";
+import useSWR from "swr";
+import "./App.css";
+import AddTodo from "./components/AddTodo";
 
-export const ENDPOINT = 'http://localhost:5173/api';
+export interface Todo {
+  id: number;
+  title: string;
+  body: string;
+  done: boolean;
+}
+
+export const ENDPOINT = "http://localhost:4000";
+
 
 const fetcher = (url: string) =>
-  fetch(`${ENDPOINT}/${url}`).then((res) => res.json());
+  fetch(`${ENDPOINT}/${url}`).then((r) => r.json());
 
 function App() {
-  const { data, mutate } = useSWR('api/todos', fetcher);
+  const { data, mutate } = useSWR<Todo[]>("api/todos", fetcher);
+
+async function markTodoAdDone(id: number) {
+  const updated = await fetch(`${ENDPOINT}/api/todos/${id}/done`, { // Fixed 'todosn' to 'todos'
+    method: "PATCH",
+  });
+
+  const json = await updated.json();
+  mutate(json);
+}
+
 
   return (
-    <MantineProvider>
-      <Box>
-        {JSON.stringify(data)}
-        <AddTodo />
-      </Box>
-    </MantineProvider>
+    <Box
+      style={{
+        padding: "2rem",
+        width: "100%",
+        maxWidth: "40rem",
+        margin: "0 auto",
+      }}
+    >
+      <List spacing="xs" size="sm" mb={12} center>
+        {data?.map((todo) => {
+          return (
+            <List.Item
+              onClick={() => markTodoAdDone(todo.id)}
+              key={`todo_list__${todo.id}`}
+              icon={
+                todo.done ? (
+                  <ThemeIcon color="teal" size={24} radius="xl">
+                    <CheckCircleFillIcon size={20} />
+                  </ThemeIcon>
+                ) : (
+                  <ThemeIcon color="gray" size={24} radius="xl">
+                    <CheckCircleFillIcon size={20} />
+                  </ThemeIcon>
+                )
+              }
+            >
+              {todo.title}
+            </List.Item>
+          );
+        })}
+      </List>
+
+      <AddTodo mutate={mutate} />
+    </Box>
   );
 }
 
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { MantineProvider } from "@mantine/core";
+import App from "./App";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const container = document.getElementById("root");
+if (!container) {
+  throw new Error("Root container missing in index.html");
+}
+
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <MantineProvider>
+      <App />
+    </MantineProvider>
+  </React.StrictMode>
+);
